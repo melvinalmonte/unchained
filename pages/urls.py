bfe714a048eea1cc95f9c5ee9e091426dfb710a7
Melvin A <melvinalmonte@gmail.com>
2023-12-09 22:57:14 -0500

    adding basic formatting

--- pages/urls.py
+++ pages/urls.py
@@ -7,11 +7,13 @@ from . import views
 
 
 def hello(request):
-    print('hello')
-    return HttpResponse(json.dumps(dict(status='ok', message='pong')), content_type='application/json')
+    print("hello")
+    return HttpResponse(
+        json.dumps(dict(status="ok", message="pong")), content_type="application/json"
+    )
 
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('health/', hello, name='health')
+    path("", views.HomeView.as_view(), name="home"),
+    path("health/", hello, name="health"),
 ]
